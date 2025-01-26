6f348b6035e7267b18cf03f6d9bf86207c3942ac
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-01-26 23:50:14 +0900

    Add cache.yaml

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction
+          Learn GithubAction 2
         </a>
       </header>
     </div>
